67d2edf2254ce1737a0345a6733cdb78ce0cefc2
seokhwan-an <asok369258@naver.com>
2025-08-08 00:10:13 +0900

    feat : 프로젝트 추가

---
+++ src/main/kotlin/oystudy/clonecoding/ClonecodingApplication.kt
@@ -0,0 +1,11 @@
+package oystudy.clonecoding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClonecodingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClonecodingApplication>(*args)
+}
---
+++ src/test/kotlin/oystudy/clonecoding/ClonecodingApplicationTests.kt
@@ -0,0 +1,13 @@
+package oystudy.clonecoding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClonecodingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
